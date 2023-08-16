868c998fb418f497e9ddea06040e35a0b157b50d
DevGamer9991 <pathornton114@gmail.com>
2023-08-16 11:50:54 -0500

    Added saving and loading

--- src/main-widget.cpp
+++ src/main-widget.cpp
@@ -3,31 +3,40 @@
 MainWidget::MainWidget(QWidget *parent) : QDockWidget("Propresenter Controller", parent) {
     this->parent = parent;
 
-    QWidget *widget = new QWidget();
-    QVBoxLayout *mainLayout = new QVBoxLayout(); // create a vertical layout
-    QGridLayout *gridLayout = new QGridLayout(); // create a grid layout
-    QPushButton *fullscreenButton = new QPushButton("FullScreen");
-    QPushButton *lowerthirdButton = new QPushButton("Lower Third");
-    QPushButton *scriptureButton = new QPushButton("Scripture View");
-    ipInput = new QLineEdit(); // create a QLineEdit for the IP input
+    ipInput = new QLineEdit();
 
     gridLayout->addWidget(fullscreenButton, 0, 0);
     gridLayout->addWidget(lowerthirdButton, 0, 1);
     gridLayout->addWidget(scriptureButton, 1, 0);
 
-    QHBoxLayout *ipLayout = new QHBoxLayout(); // create a horizontal layout for the IP input and Set IP button
-    ipLayout->addWidget(ipInput); // add the IP input to the layout
+    QHBoxLayout *ipLayout = new QHBoxLayout(); 
+    ipLayout->addWidget(ipLabel);
+    ipLayout->addWidget(ipInput); 
 
-    mainLayout->addLayout(gridLayout); // add the grid layout to the vertical layout
-    mainLayout->addLayout(ipLayout); // add the IP input and Set IP button layout to the vertical layout
+    mainLayout->addLayout(gridLayout);
+    mainLayout->addLayout(ipLayout); 
 
-    widget->setLayout(mainLayout); // set the main layout as the widget's layout
+    widget->setLayout(mainLayout);
 
     setWidget(widget);
 
     setVisible(false);
     setFloating(true);
     resize(300, 300);
+
+    std::string profilePath = obs_frontend_get_current_profile_path();
+    profilePath.append("/obs-propresenter.json");
+
+    // get the value of the ip from the obs data file if it exists
+    obs_data_t *settings = obs_data_create_from_json_file_safe(profilePath.c_str(), "bak");
+    if (settings) {
+        const char *ip = obs_data_get_string(settings, "ip");
+        if (ip) {
+            ipInput->setText(ip);
+        }
+    }
+
+    obs_data_release(settings);
 
     manager = new QNetworkAccessManager();
     QObject::connect(manager, &QNetworkAccessManager::finished,
@@ -41,6 +50,8 @@ MainWidget::MainWidget(QWidget *parent) : QDockWidget("Propresenter Controller",
         }
     );
 
+    QObject::connect(ipInput, SIGNAL(editingFinished()), SLOT(ipInputEdited()));
+
     QObject::connect(fullscreenButton, SIGNAL(clicked()), SLOT(FullscreenButtonClicked()));
     QObject::connect(lowerthirdButton, SIGNAL(clicked()), SLOT(lowerthirdButtonClicked()));
     QObject::connect(scriptureButton, SIGNAL(clicked()), SLOT(scriptureButtonClicked()));
@@ -49,6 +60,21 @@ MainWidget::MainWidget(QWidget *parent) : QDockWidget("Propresenter Controller",
 MainWidget::~MainWidget() {
     delete manager;
 };
+
+void MainWidget::ipInputEdited() {
+    // save the ipText to the obs data file
+    obs_data_t *settings = obs_data_create();
+    obs_data_set_string(settings, "ip", ipInput->text().toStdString().c_str());
+    // now save it to json
+
+    //get a string from the obs_frontend_get_current_profile_path()
+    //then append the file name to it then save it
+    std::string profilePath = obs_frontend_get_current_profile_path();
+    profilePath.append("/obs-propresenter.json");
+
+    obs_data_save_json(settings, profilePath.c_str());
+    obs_data_release(settings);
+}
 
 void MainWidget::FullscreenButtonClicked() {
     request.setUrl(QUrl("http://" + ipInput->text() + ":1025/v1/look/0/trigger"));
